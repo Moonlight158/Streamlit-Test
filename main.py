1cc640192bd963a35bb8c7286c5e948c8ed695bb
Moonlight158 <bobjoa0111@naver.com>
2023-05-30 17:24:21 +0900

    이항 분포 그래프 테스트
    코드 정상 동작 테스트용 커밋

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 # main.py
 import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+from math import factorial
 
-st.text('hello Streamlit!')
+def bin_dist(k, n, p):
+    nck = factorial(n) / (factorial(k) * factorial(n-k))
+    pd = nck * p**k * (1-p)**(n-k)
+    return pd
+
+def main():
+    # input number
+    st.title('이항 분포 시뮬레이터')
+    number = st.number_input('값을 입력해주세요!', 1, help='ex) 1, 10')
+    p = st.number_input('확률 값을 입력해주세요!', 0.3, help='ex) 0.1, 0.3')
+
+    x = np.arange(number + 1)
+    pd1 = np.array([bin_dist(k, number, p) for k in range(number + 1)])
+    fig, ax = plt.subplots()
+    ax.plot(x, pd1)
+    ax.set_title('n = {}, p={}'.format(number, p))
+    st.pyplot(fig)
+
+if __name__ == "__main__":
+    main()
