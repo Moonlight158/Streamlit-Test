5c6ff7e224f9749a90e3942eae48f37cd976575a
Moonlight158 <bobjoa0111@naver.com>
2023-05-31 02:22:36 +0900

    그래프 값 확인을 위해 패키지 변경하여 구현
    마우스를 올렸을 때 그래프 값 확인 가능하도록 그래프 시각화 패키지 변경
    (matplotlib → plotly)

---
+++ main_plotly.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import plotly.graph_objects as go
+import numpy as np
+from math import factorial
+
+def bin_dist(k, n, p):
+    nck = factorial(n) / (factorial(k) * factorial(n-k))
+    pd = nck * p**k * (1-p)**(n-k)
+    return pd
+
+def main():
+    st.title('이항 분포 시뮬레이터')
+    n = st.number_input('총 시도 횟수를 입력해주세요! (1, 2, 3...)', 1, help='ex) 1, 10')
+    # 소수점 입력 시 소수점 아래 4자리를 넘어갈 경우 제대로 입력되지 않는 부분 보정하고자
+    # string형태로 확률 값을 받고 string to int로 변환하여 사용함
+    p_string = st.text_input('성공 확률을 입력해주세요! (100% = 1.0)', '0.1', help='ex) 0.1, 0.3')
+    p = float(p_string)
+
+    xList = np.arange(n+1)
+    yList = np.array(np.array([bin_dist(k, n, p) for k in range(n + 1)]))
+
+    # 그래프 객체 생성
+    fig = go.Figure()
+    fig.add_trace(go.Bar(x=xList, y=yList))
+    st.plotly_chart(fig)
+
+    helptip = '- 표의 우측 하단을 잡고 당기거나 표 영역에서 스크롤을 내리면 더 많은 정보를 볼 수 있어요!'
+    st.title('※ 확률값 상세', help=helptip)
+    st.write(yList)
+
+if __name__ == "__main__":
+    main()
