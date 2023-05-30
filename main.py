4d2bd0fb40a6a1fda41d6540ae60a536c3400364
Moonlight158 <bobjoa0111@naver.com>
2023-05-30 18:58:13 +0900

    상세 정보 보기 추가
    1. 검산용 값보기 기능 추가
    2. x, y축 레이블 추가
    3. 입력값에 대한 가이드 추가

--- main.py
+++ main.py
@@ -11,16 +11,24 @@ def bin_dist(k, n, p):
 
 def main():
     # input number
-    st.title('이항 분포 시뮬레이터')
-    number = st.number_input('값을 입력해주세요!', 1, help='ex) 1, 10')
-    p = st.number_input('확률 값을 입력해주세요!', 0.3, help='ex) 0.1, 0.3')
+    plt.rcParams['font.family'] = 'Malgun Gothic'
 
-    x = np.arange(number + 1)
-    pd1 = np.array([bin_dist(k, number, p) for k in range(number + 1)])
+    st.title('이항 분포 시뮬레이터')
+    n = st.number_input('총 시도 횟수를 입력해주세요! (1, 2, 3...)', 1, help='ex) 1, 10')
+    p = st.number_input('성공 확률을 입력해주세요! (100% = 1.0)', 0.5, help='ex) 0.1, 0.3')
+
+    x = np.arange(n + 1)
+    pd1 = np.array([bin_dist(k, n, p) for k in range(n + 1)])
     fig, ax = plt.subplots()
     ax.plot(x, pd1)
-    ax.set_title('n = {}, p={}'.format(number, p))
+    ax.set_title('n = {}, p={}'.format(n, p))
+    ax.set_xlabel('성공 횟수',fontdict={'fontname':'Malgun Gothic'})
+    ax.set_ylabel('기대 확률',fontdict={'fontname':'Malgun Gothic'})
     st.pyplot(fig)
+
+    helptip = '- 표의 우측 하단을 잡고 당기거나 표 영역에서 스크롤을 내리면 더 많은 정보를 볼 수 있어요!'
+    st.title('※ 확률값 상세', help=helptip)
+    st.write(pd1)
 
 if __name__ == "__main__":
     main()
